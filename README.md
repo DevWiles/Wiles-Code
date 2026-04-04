212bebec1fff2bd75b83d5fa07308264f10f3175
Wiles <nathan.demon66@gmail.com>
2026-04-05 01:03:58 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wiles Code CLI
+一个基于 LangChain 的交互式代码助手 CLI 工具，支持流式输出、工具调用和智能对话。
---
+++ src/main/java/agent/wilescode/WilesCodeApplication.java
@@ -0,0 +1,13 @@
+package agent.wilescode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilesCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WilesCodeApplication.class, args);
+    }
+
+}
